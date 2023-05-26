1a65096b0bdffe7844f64d650c5a5432507f11e0
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-05-26 20:15:51 +0800

    Add def for operations and a new folder for test(Input for two numbers that will user give)

---
+++ Test-Calculator-OOP.py
@@ -0,0 +1,9 @@
+from Calculator-OOP import calculator
+
+numbers = calculator(num1, num2)
+
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+
+    
