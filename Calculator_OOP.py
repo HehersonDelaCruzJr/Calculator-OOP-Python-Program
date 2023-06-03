f38b268505cd82f4d30b25093f6db4882d379edc
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-06-03 23:54:54 +0800

    inheritance implementation

--- Calculator_OOP.py
+++ Calculator_OOP.py
@@ -1,8 +1,10 @@
-class Calculator():
+
+
+class Calculator:   
     def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
-
+        
     def addition(self):
         return self.num1 + self.num2
 
@@ -15,8 +17,12 @@ class Calculator():
     def division(self):
         return self.num1 / self.num2
 
+   
+class operators(Calculator):
+    def printSelf(result):
+        print("Result: ", result)
 
-calculator = Calculator(0, 0)
+operator = operators(0, 0)
 
 selector = 1
 while selector != 0:
@@ -24,6 +30,7 @@ while selector != 0:
     print("2. Subtraction")
     print("3. Multiplication")
     print("4. Division")
+    
     print("5. Exit")
 
     selector = float(input("Enter your choice: "))
@@ -31,30 +38,31 @@ while selector != 0:
     if selector == 1:
         num1 = float(input("Enter your first number: "))
         num2 = float(input("Enter your second number: "))
-        calculator.num1 = num1
-        calculator.num2 = num2
-        print("Result: ", calculator.addition())
+    
+        operator.num1 = num1
+        operator.num2 = num2
+        operators.printSelf(operator.addition())
 
     elif selector == 2:
         num1 = float(input("Enter your first number: "))
         num2 = float(input("Enter your second number: "))
-        calculator.num1 = num1
-        calculator.num2 = num2
-        print("Result: ", calculator.subtraction())
+        operator.num1 = num1
+        operator.num2 = num2
+        operators.printSelf(operator.subtraction())
 
     elif selector == 3:
         num1 = float(input("Enter your first number: "))
         num2 = float(input("Enter your second number: "))
-        calculator.num1 = num1
-        calculator.num2 = num2
-        print("Result: ", calculator.multiplication())
+        operator.num1 = num1
+        operator.num2 = num2
+        operators.printSelf(operator.multiplication())
 
     elif selector == 4:
         num1 = float(input("Enter your first number: "))
         num2 = float(input("Enter your second number: "))
-        calculator.num1 = num1
-        calculator.num2 = num2
-        print("Result: ", calculator.division())
+        operator.num1 = num1
+        operator.num2 = num2
+        operators.printSelf(operator.division())
 
     elif selector == 5:
         print("Exit")
